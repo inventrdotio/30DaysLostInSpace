bcf4060623468a5eaf74c0ae6ea5b988ab1eba72
David Schmidt <davids@tower-mt.com>
2023-09-05 09:53:34 -0700

    Fix comment in Day 7 sketch

--- Day07-We_Still_Need_To_Keep_An_Eye_On_This/Day-07/Day-07.ino
+++ Day07-We_Still_Need_To_Keep_An_Eye_On_This/Day-07/Day-07.ino
@@ -1,9 +1,10 @@
 // Code written by Alexander Eschenauer for the 30 Day Adventure Kit
 // Learn more at https://inventr.io/adventure
 
-int sensorPin = A0;   // select the input pin for the potentiometer
+int sensorPin = A0;   // select the input pin for the photoresistor
 int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
+
 unsigned int batteryCapacity = 50000;
 unsigned int batteryLevel = 0;
 

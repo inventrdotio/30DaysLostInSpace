930ee8d940e5bf3e52fc82f753892c071706b892
David Schmidt <davids@tower-mt.com>
2023-09-18 11:14:16 -0700

    Renamed Day 4 constants to match Astrid video

--- Day04-Cabin_Lighting/Day-04/Day-04.ino
+++ Day04-Cabin_Lighting/Day-04/Day-04.ino
@@ -4,10 +4,10 @@
  *
  * Learn more at https://inventr.io/adventure
  *
- * Since our lander has more than one light, let's extend our previous project by
- * adding multiple lights and use all three switches on our DIP switch module.  Each
- * time through our loop() code we'll check each switch, and set the corresponding
- * light's state appropriately.
+ * In addition to our cabin lights we have lights in the storage room and even some
+ * exterior lights.  Let's extend our previous project by adding the others and use
+ * all three switches on our DIP switch module.  Each time through our loop() code we'll
+ * check each switch, and set the corresponding light's state appropriately.
  *
  * Alex Eschenauer
  * David Schmidt
@@ -27,49 +27,49 @@
 // Each lander light will be controlled using a different pin on our HERO.
 // We will use three different color LEDs to make it more clear which switch
 // controls which light.
-const uint8_t RED_LED_PIN = 10;    // pin controlling the Red LED
-const uint8_t BLUE_LED_PIN = 11;   // pin controlling the Blue LED
-const uint8_t GREEN_LED_PIN = 12;  // pin controlling the Green LED
+const uint8_t CABIN_LIGHTS_PIN = 10;    // pin controlling the cabin lights
+const uint8_t STORAGE_LIGHTS_PIN = 11;   // pin controlling the storage lights
+const uint8_t COCKPIT_LIGHTS_PIN = 12;  // pin controlling the exterior lights
 
 // We will use a different input pin for each of our DIP switches and
 // we'll label them using the color of the LED that each switch will control.
-const uint8_t RED_LED_SWITCH_PIN = 2;    // Red LED controlled by switch 1
-const uint8_t BLUE_LED_SWITCH_PIN = 3;   // Blue LED controlled by switch 2
-const uint8_t GREEN_LED_SWITCH_PIN = 4;  // Green LED controlled by switch 3
+const uint8_t CABIN_LIGHTS_SWITCH_PIN = 2;    // cabin lights controlled by switch 1
+const uint8_t STORAGE_LIGHTS_SWITCH_PIN = 3;   // storage lights controlled by switch 2
+const uint8_t COCKPIT_LIGHTS_SWITCH_PIN = 4;  // exterior lights controlled by switch 3
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // Configure our LED control pins as OUTPUT pins
-  pinMode(RED_LED_PIN, OUTPUT);    // pin controlling the red LED
-  pinMode(BLUE_LED_PIN, OUTPUT);   // pin controlling the blue LED
-  pinMode(GREEN_LED_PIN, OUTPUT);  // pin controlling the green LED
+  pinMode(CABIN_LIGHTS_PIN, OUTPUT);    // pin controlling the cabin lights
+  pinMode(STORAGE_LIGHTS_PIN, OUTPUT);   // pin controlling the storage lights
+  pinMode(COCKPIT_LIGHTS_PIN, OUTPUT);  // pin controlling the exterior lights
   // Configure the switch pins as INPUT pins
-  pinMode(RED_LED_SWITCH_PIN, INPUT);    // pin connected to switch 1 (red LED)
-  pinMode(BLUE_LED_SWITCH_PIN, INPUT);   // pin connected to switch 2 (blue LED)
-  pinMode(GREEN_LED_SWITCH_PIN, INPUT);  // pin connected to switch 3 (green LED)
+  pinMode(CABIN_LIGHTS_SWITCH_PIN, INPUT);    // pin connected to switch 1 (cabin lights)
+  pinMode(STORAGE_LIGHTS_SWITCH_PIN, INPUT);   // pin connected to switch 2 (storage lights)
+  pinMode(COCKPIT_LIGHTS_SWITCH_PIN, INPUT);  // pin connected to switch 3 (exterior lights)
 }
 
 // Each time through loop() we will check each switch in turn and set each light's
 // state appropriately.
 void loop() {
-  // Control red LED based on setting on switch 1
-  if (digitalRead(RED_LED_SWITCH_PIN) == HIGH) {  // if switch is on (HIGH voltage)
-    digitalWrite(RED_LED_PIN, HIGH);              // turn on LED by providing HIGH voltage (5v)
+  // Control cabin lights based on setting on switch 1
+  if (digitalRead(CABIN_LIGHTS_SWITCH_PIN) == HIGH) {  // if switch is on (HIGH voltage)
+    digitalWrite(CABIN_LIGHTS_PIN, HIGH);              // turn on LED by providing HIGH voltage (5v)
   } else {
-    digitalWrite(RED_LED_PIN, LOW);  // turn off LED by setting output to LOW (zero volts)
+    digitalWrite(CABIN_LIGHTS_PIN, LOW);  // turn off LED by setting output to LOW (zero volts)
   }
 
-  // Control blue LED based on setting on switch 2
-  if (digitalRead(BLUE_LED_SWITCH_PIN) == HIGH) {  // if switch is on (HIGH voltage)
-    digitalWrite(BLUE_LED_PIN, HIGH);              // turn on LED by providing HIGH voltage (5v)
+  // Control storage lights based on setting on switch 2
+  if (digitalRead(STORAGE_LIGHTS_SWITCH_PIN) == HIGH) {  // if switch is on (HIGH voltage)
+    digitalWrite(STORAGE_LIGHTS_PIN, HIGH);              // turn on LED by providing HIGH voltage (5v)
   } else {
-    digitalWrite(BLUE_LED_PIN, LOW);  // turn off LED by setting output to LOW (zero volts)
+    digitalWrite(STORAGE_LIGHTS_PIN, LOW);  // turn off LED by setting output to LOW (zero volts)
   }
 
-  // Control green LED based on setting on switch 3
-  if (digitalRead(GREEN_LED_SWITCH_PIN) == HIGH) {  // if switch is on (HIGH voltage)
-    digitalWrite(GREEN_LED_PIN, HIGH);              // turn the LED on (HIGH is the voltage level)
+  // Control exterior lights based on setting on switch 3
+  if (digitalRead(COCKPIT_LIGHTS_SWITCH_PIN) == HIGH) {  // if switch is on (HIGH voltage)
+    digitalWrite(COCKPIT_LIGHTS_PIN, HIGH);              // turn the LED on (HIGH is the voltage level)
   } else {
-    digitalWrite(GREEN_LED_PIN, LOW);  // turn off LED by setting output to LOW (zero volts)
+    digitalWrite(COCKPIT_LIGHTS_PIN, LOW);  // turn off LED by setting output to LOW (zero volts)
   }
 }

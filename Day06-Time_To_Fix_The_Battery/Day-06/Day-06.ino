180108d67edf1edd25896c45d98b9ecfce0dda49
InventrKits <120519839+inventrdotio@users.noreply.github.com>
2023-09-19 14:25:11 -0400

    Update Day-06.ino

--- Day06-Time_To_Fix_The_Battery/Day-06/Day-06.ino
+++ Day06-Time_To_Fix_The_Battery/Day-06/Day-06.ino
@@ -1,21 +1,21 @@
 // Code written by Alexander Eschenauer for the 30 Day Adventure Kit
 // Learn more at https://inventr.io/adventure
  
-int sensorPin = A0;   // select the input pin for the photoresistor
-int onboardLED = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
+int PHOTORESISTOR_PIN = A0;   // select the input pin for the photoresistor
  
 void setup() {
-  pinMode(onboardLED, OUTPUT);
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT); // Set onboard LED as an OUTPUT
+  Serial.begin(9600); // Talk to computer over baud rate 9600
 }
  
 void loop() {
-  sensorValue = analogRead(sensorPin);
-  digitalWrite(onboardLED, HIGH);
-  delay(sensorValue);
-  digitalWrite(onboardLED, LOW);
-  delay(sensorValue);
-  Serial.println(sensorValue);
-  delay(500);
+  int ROOM_BRIGHTNESS = analogRead(PHOTORESISTOR_PIN); // Read value from photoresistor
+
+  digitalWrite(LED_BUILTIN, HIGH); // Turn onboard LED on
+  delay(ROOM_BRIGHTNESS); // wait
+  digitalWrite(LED_BUILTIN, LOW); // Turn onboard LED off
+  delay(ROOM_BRIGHTNESS); // wait
+
+  Serial.println(ROOM_BRIGHTNESS); // print value back to computer
+  delay(50); // short delay to help de-clutter serial monitor
 }

d17586aeed1b9db17935d5995c1d79877606bf14
David Schmidt <davids@tower-mt.com>
2023-10-04 20:30:29 -0700

    Move final Day 6, Day 7 to proper folders

--- Day06-Time_To_Fix_The_Battery/Day-06/Day-06.ino
+++ Day06-Time_To_Fix_The_Battery/Day-06/Day-06.ino
@@ -1,21 +1,151 @@
-// Code written by Alexander Eschenauer for the 30 Day Adventure Kit
-// Learn more at https://inventr.io/adventure
- 
-#define PHOTORESISTOR_PIN = A0;   // select the input pin for the photoresistor
+/*
+ * 30 Days - Lost in Space
+ * Day 6 - Time to Fix the Battery
+ *
+ * Learn more at https://inventr.io/adventure
+ *
+ * We will need to find an additional source of power as our battery will not last long
+ * without some recharging.  We do have solar panels so let's work on how the HERO can
+ * monitor the power coming from those panels using a "photo-resistor" to simulate the
+ * variable power available to our Lander.
+ *
+ * Alex Eschenauer
+ * David Schmidt
+ * Greg Lyzenga
+ */
 
+/*
+ * Arduino concepts introduced/documented in this lesson.
+ * - unsigned int: A 16 bit value containing numbers from 0 to 65535
+ * - Serial Monitor: Allows the HERO to display text in an Arduino IDE window.
+ * - Serial.begin(): Used to initialize the Serial Monitor.
+ * - Serial.print(): Display some text in the Arduino IDE Serial Monitor window.
+ * - Serial.println(): Display test in the Serial Monitor followed by a newline.
+ * - analogRead(): Read a value from an analog pin that is based on how much voltage is on the pin (0-5v)
+ *
+ * Parts and electronics concepts introduced in this lesson.
+ * - Analog pins: Pins on the HERO that can read many different values instead of just HIGH/LOW.
+ * - Photo Resistor: Changes it's resistance depending on how much light it senses.
+ */
+
+/* We start by including this line of code, which helps our HERO work properly with the Arduino program. */
+#include "Arduino.h"
+
+// Our photoresistor will give us a reading of the current light level on this analog pin
+const byte PHOTORESISTOR_PIN = A0;  // we pick an analog pin (defined in Arduino.h)
+
+// These two constants set the minimum and maximum delay times for oue blinking LED
+// The type "unsigned int" represents numbers from 0 to 65535.  Another name you may
+// see for this type is "uint16_t".  We need this since these delay values can be
+// greater than the biggest number a byte can represent (255).
+const unsigned int MIN_DELAY = 50;   // 50 ms shortest blink delay
+const unsigned int MAX_DELAY = 500;  // 500 ms longest blink delay
+
+// One time setup
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT); // Set onboard LED as an OUTPUT
-  Serial.begin(9600); // Talk to computer over baud rate 9600
+  // We will blink our build in LED based on amount of light received from our photoresistor
+  pinMode(LED_BUILTIN, OUTPUT);       // output since we will control the built in LED
+  pinMode(PHOTORESISTOR_PIN, INPUT);  // input value from analog pin connected to photoresistor
+
+  /*
+   * To show you the exact value being read on the analog pin we will print the exact number
+   * using our Arduino IDE's "Serial Monitor".  This is a window displayed under the sketch that
+   * can display text sent to it from the HERO.
+   *
+   * The speed that this data is sent/received must match between the Arduino IDE and HERO.  We
+   * configure this speed for the HERO to send data using the Serial.begin() function.  Throughout
+   * this course we will use a typical value of 9600 "baud", which is 9,600 bit of information per
+   * second.
+   */
+  Serial.begin(9600);  // This initializes the Serial Monitor and sets the speed to 9600 bits per second
 }
 
+// The loop() function is called over and over when sketch is run.
 void loop() {
-  int roomBrightness = analogRead(PHOTORESISTOR_PIN); // Read value from photoresistor
-  roomBrightness = roomBrightness * 10; // Make the delay longer so you can actually see the change in blinking delays
-  
-  digitalWrite(LED_BUILTIN, HIGH); // Turn onboard LED on
-  delay(roomBrightness); // wait
-  digitalWrite(LED_BUILTIN, LOW); // Turn onboard LED off
-  delay(roomBrightness); // wait
-  
-  Serial.println(roomBrightness); // print value back to computer
+  /*
+   * Each time through our loop() we will read the current value of our photoresistor.  When
+   * the voltage goes up from more light we flash the built-in LED faster (with shorter blinks).
+   *
+   * The photoresistor will produce a voltage that is not a simple digitial (on/off, HIGH/LOW)
+   * value.  Because of this we cannot use a digital pin on our HERO.  Luckily our HERO board
+   * provides six "analog" pins, named A0-A5.
+   *
+   * These pins will convert a voltage from 0V to 5V to a number from 0 to 1023, giving us
+   * a full range of values.  Because we are reading an analog value instead of a digital
+   * value, we use the analogRead() function.
+   *
+   * Here we use the reading from the PHOTORESISTOR_PIN and modify how long we delay based on it.
+   */
+  unsigned int light_value = analogRead(PHOTORESISTOR_PIN);   // light value from 0 to 1024
+
+  /*
+   * Our HERO board doesn't have a text display, so the Arduino IDE has provided us a way to
+   * for the HERO to send messages that are displayed in a window in the Arduino IDE.  To open
+   * the Serial Monitor on the Arduino IDE you can select Tools/Serial Monitor from the Arduino
+   * IDE menu, use the shortcut keys listed in the menu or click the "magnifying glass" icon in
+   * the upper right of the IDE window.
+   *
+   * We initialized the Serial functions in setup and now we will use Serial.print() and
+   * Serial.println() to send messages to the Arduino IDE Serial Monitor.  Both functions
+   * take a single value which can be either a constant or a variable.  The only difference
+   * between the two functions is that Serial.print() leaves the cursor at the end of the
+   * current line.  Any additional output will be appended to the current line.  Serial.println()
+   * prints the value and THEN adds a "newline", so that any following messages will appear
+   * on the following line.
+   */
+  Serial.print("Light value: ");  // Display label string to serial monitor
+  Serial.print(light_value);      // display the value read from our photoresistor
+
+  /*
+   * The flash rate varies based on the relative brightness received by the photoresistor.
+   * However, your brightness levels could be very different if you are in a dark room from
+   * running this in a bright sunny room.  Because of this we will save the darkest value
+   * and the brightest value seen.
+   *
+   * These values would normally be lost when each run of our loop() ends, but by adding
+   * the "static" declaration we indicate that these local variables should maintain their
+   * values between loop() runs.
+   *
+   * The FIRST time these variables are used they will get initialized with the first light_value
+   * but that initialization is only done the first time so they will retain any changes
+   * between the loop() runs.
+   */
+  static unsigned int darkest_light = light_value;    // this is the lowest value returned by the photoresistor
+  static unsigned int brightest_light = light_value;  // this is the highest value returned by the photoresistor
+
+  /*
+   * Now that we have a light value let's update our darkest and brightest values
+   * if the current value is darker (less than) our previous darkest or brighter
+   * (greater than) our previous brightest value.
+   */
+  if (light_value < darkest_light) {  // If value is LESS THAN ('<') the darkest...
+    darkest_light = light_value;      // ...then save current value as our new darkest value
+  }
+  if (light_value > brightest_light) {  // If value is GREATER THAN ('>') the brightest...
+    brightest_light = light_value;      // ...then save current value as our new brightest value
+  }
+
+  /*
+   * Now we have an interesting problem.  As our voltage reading goes up, we want the LED to
+   * blink faster (and thus with a smaller delay).  In addition, we need to adjust the blink
+   * rate in a noticeable way.  So, we have one range of numbers (darkest to brightest values)
+   * from our light sensor that we wish to convert to a different range that goes from MAX_DELAY
+   * down to MIN_DELAY.
+   *
+   * The Arduino language provides a neat function named map() that was created just for this
+   * situation.  It takes a value from one range and *maps* it into a desired range.  It properly
+   * handles the situation where the second range goes DOWN when the first range goes UP.
+   *
+   * We introduce it's used here to map our light_value (which goes from darkest_light to
+   * brightest_light) and return a value from MAX_DELAY *down* to MIN_DELAY.  Perfect!
+   */
+  unsigned int delay_value = map(light_value, darkest_light, brightest_light, MAX_DELAY, MIN_DELAY);
+  Serial.print(", Delay value: ");  // display label after light_value
+  Serial.println(delay_value);      // display delay_value returned by map() function with newline
+
+  // now blink our built in LED using our delay_value.
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn on LED
+  delay(delay_value);               // Leave on delay_value milliseconds
+  digitalWrite(LED_BUILTIN, LOW);   // Turn off LED
+  delay(delay_value);               // Leave off delay_value milliseconds
 }
--- Day07-We_Still_Need_To_Keep_An_Eye_On_This/Day-07/Day-07.ino
+++ Day07-We_Still_Need_To_Keep_An_Eye_On_This/Day-07/Day-07.ino
@@ -1,35 +1,101 @@
-// Code written by Alexander Eschenauer for the 30 Day Adventure Kit
-// Learn more at https://inventr.io/adventure
+/*
+ * 30 Days - Lost in Space
+ * Day 7 - We still need to keep an eye on this
+ *
+ * Learn more at https://inventr.io/adventure
+ *
+ * Now we will use the circuit from Day 6 and use the value read from the photoresistor
+ * to simulate charging our lander's battery.  Our battery is currently drained
+ * (current level is 0) and we will stop charging once the battery is full.
+ *
+ * Alex Eschenauer
+ * David Schmidt
+ */
 
-int sensorPin = A0;   // select the input pin for the photoresistor
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
+/*
+ * Arduino concepts introduced/documented in this lesson.
+ * - "+=" operator.
+ * - defining our own functions.
+ *
+ * Parts and electronics concepts introduced in this lesson.
+ * - No new parts as this sketch uses the same circuit as Day 6
+ */
 
-unsigned int batteryCapacity = 50000;
-unsigned int batteryLevel = 0;
+// Explicitly include Arduiono.h
+#include "Arduino.h"
 
-void PrintBatteryPercentage() {
-  Serial.print(((double)batteryLevel / (double)batteryCapacity)*100);
-  Serial.println("%");
-}
+// Our photoresistor will give us a reading of the current light level on this analog pin
+const byte PHOTORESISTOR_PIN = A0;  // Photoresistor analog pin
+
+// Use an unsigned, 16 bit value (0 - 65535) for maximum battery capacity
+const unsigned int BATTERY_CAPACITY = 50000;  // Maximum battery capacity
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
+  pinMode(PHOTORESISTOR_PIN, INPUT);  // Input current light level from photoresistor
+
+  Serial.begin(9600);  // Initialize Serial Monitor to 9600 baud
 }
 
+// Use global here because this variable is accessed by both loop() and our custom
+// function.  (In later lessons we will show how to avoid globals.)
+unsigned int battery_level = 0;  // Current battery charge level (set to 0 first time)
+
+/*
+ * Each time through our loop() we read the current light level from the photoresistor
+ * and add that to our current battery level to simulate charging our battery.
+ *
+ * We then display current charge level on the Serial Monitor.
+ */
 void loop() {
-  sensorValue = analogRead(sensorPin); // read the value from the sensor:
-  batteryLevel += sensorValue;
-  
-  if(batteryLevel >= batteryCapacity) {
-    Serial.println("FULLY CHARGED");
-    batteryLevel = batteryCapacity; // to prevent integer overflow errors
+
+  // Display battery information on the Serial Monitor
+  if (battery_level < BATTERY_CAPACITY) {  // If battery is not fully charged..
+    /*
+     * Here we introduce a new arithmetic operator, "+=".
+     * When you wish to add a value to an existing variable you *could* write it like
+     * "value = value + additional;".  But the Arduino C++ language allows you to avoid
+     * the duplication.  The += operator allows us to add to an existing variable value.
+     *
+     * In the following statement we will add our current light level to our existing
+     * battery level.
+     */
+    battery_level += analogRead(PHOTORESISTOR_PIN);  // read light level and add to battery level
+    if (battery_level > BATTERY_CAPACITY) {          // If this would make charge over 100%...
+      battery_level = BATTERY_CAPACITY;              // ...set current level to maximum capacity.
+    }
   }
-  else {
-    PrintBatteryPercentage();
+
+  /*
+   * Previously we have only used functions included in the Arduino C++ Language.  Here
+   * we demonstrate how we can use our own functions.  This keeps our main code cleaner
+   * and easier to understand.  All you need to know here is that this new function prints
+   * the current charge level on the Serial Monitor.
+   *
+   * If you need to know the details you can examine the full function declaration following
+   * our loop() code.
+   */
+  printBatteryChargeLevel();  // Display current charge percentage on Serial Monitor
+
+  delay(100);
+}
+
+/*
+ * This is where our own function is defined to display the current battery charge
+ * level on the Serial Monitor.
+ *
+ * This function takes no parameters and returns no values (indicated by the "void" preceeding the name).
+ *
+ * If the battery is not fully charged yet we then print the current battery percentage
+ * to the Serial Monitor.
+ *
+ * If the battery is fully charged we display that to the serial monitor.
+ */
+void printBatteryChargeLevel() {
+  if (battery_level < BATTERY_CAPACITY) {  // if not fully charged
+    // Percentage of charge is current level divided by capacity, multiplied by 100 to get a percentage.
+    Serial.print(((double)battery_level / (double)BATTERY_CAPACITY) * 100);  // display charge % to Serial Monitor
+    Serial.println("%");
+  } else {
+    Serial.println("FULLY CHARGED");  // ...indicate fully charged on Serial Monitor
   }
-  
-  delay(50);
 }

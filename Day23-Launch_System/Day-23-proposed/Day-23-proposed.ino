417e3ddd5ae5afb9422e48bcdcdebfe20041faff
David Schmidt <davids@tower-mt.com>
2023-11-01 10:35:24 -0700

    final Day 23 proposed sketch pending review

--- Day23-Launch_System/Day-23-proposed/Day-23-proposed.ino
+++ Day23-Launch_System/Day-23-proposed/Day-23-proposed.ino
@@ -19,9 +19,10 @@
 
 /*
  * Arduino concepts introduced/documented in this lesson.
- * - 
- * - 
- * - 
+ * - #define macros
+ * - rounding up calculated values
+ * - elapsed time without using delay() calls.
+ * - firstPage()/nextPage() graphics loop to save memory
  *
  * Parts and electronics concepts introduced in this lesson.
  * - 
@@ -48,26 +49,43 @@
  * it's still often used, but on Day 4 we introduced the newer and better "const"
  * and have been using it since to define all of our constants.
  *
- * But "#define" *does* have some capabilities that "const" doesn't, and we're going
- * to use of those today.  
+ * But "#define" *does* have some capabilities that "const" doesn't.  The one we
+ * use today is the use of parameters, turning our constant into a simple, in-line
+ * function.
+ *
+ * Since #define performs simple text substitution this method executes faster since
+ * it doesn't have any overhead to call and return a function.
+ *
+ * WARNING: #define macros CAN cause bugs if the variable passed in is used in multiple
+ * places in the macro because the text inside the call is simply replaced in the macro.
+ *
+ * These examples perform a simple calculation returning the minutes or seconds from
+ * a number representing milliseconds, rounded UP to the next highest second by adding
+ * 999 milliseconds.  This allows our counter to return 00:01 until the instant the
+ * counter reaches zero.
  */
-#define numberOfMinutes(_time_) (((_time_ + 999) / 1000) / 60)
-#define numberOfSeconds(_time_) (((_time_ + 999) / 1000) % 60)
+#define numberOfMinutes(_milliseconds_) (((_milliseconds_ + 999) / 1000) / 60)
+#define numberOfSeconds(_milliseconds_) (((_milliseconds_ + 999) / 1000) % 60)
 
-// 7 SEGMENT PIN ASSIGNMENTS
-#define CLKsev 5
-#define DIOsev 4
+// Pin connections for our 4 digit counter
+#define COUNTER_DISPLAY_CLK_PIN 5
+#define COUNTER_DISPLAY_DIO_PIN 4
+// Construct counter_display handle.
+TM1637Display counter_display(COUNTER_DISPLAY_CLK_PIN, COUNTER_DISPLAY_DIO_PIN);
 
-// DEFINE DISPLAY OBJECTS
-// Construct our lander_display handle using the same constructor from Day 21
+/*
+ * This constructor for our OLED differs from the one used for the previous
+ * days, changing the "_F_" to "_2_".  This indicates that instead of using
+ * a full sized buffer to draw to our OLED display a smaller, 256 byte buffer
+ * will be used along with the firstPage()/nextPage() calls.  This gives us
+ * more memory for other variables, though the display may update slower.
+ */
 U8G2_SH1106_128X64_NONAME_2_HW_I2C lander_display(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
-TM1637Display counter_display(5, 4);
 
 const byte LANDER_HEIGHT = 25;  // height of our lander image, in bits
 const byte LANDER_WIDTH = 20;   // width of our lander image, in bits
 
-// HANDY CONSTANTS
-// const uint8_t OFF[] = { 0, 0, 0, 0 };
+// Define 7 Segment display values to spell out "dOnE"
 const uint8_t DONE[] = {
   SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,          // d
   SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,  // O
@@ -75,37 +93,60 @@ const uint8_t DONE[] = {
   SEG_A | SEG_D | SEG_E | SEG_F | SEG_G           // E
 };
 
-// GLOBAL VARIABLES
-const unsigned long COUNTDOWN_TIME = 70000;
+// Define amount of time (in milliseconds) to count down.
+const unsigned long COUNTDOWN_MILLISECONDS = 5 * 1000;
 
 // *********************************************
 void setup() {
   Serial.begin(9600);
-  // Set brightness
-  counter_display.setBrightness(7);
-  // Clear the display
-  counter_display.clear();
 
-  lander_display.begin();
-  lander_display.setFont(u8g2_font_6x10_tr);
-  // lander_display.setFont(u8g2_font_tallpixelextended_tr);
-  lander_display.setFontRefHeightExtendedText();
+  // Configure counter display
+  counter_display.setBrightness(7);  // Set maximum brightness (value is 0-7)
+  counter_display.clear();           // Clear the display
+
+  // Configure OLED display
+  lander_display.begin();                     // Initialize OLED display
+  lander_display.setFont(u8g2_font_6x10_tr);  // Set text font
+  lander_display.setFontRefHeightText();
   lander_display.setFontPosTop();
 
+  /*
+   * Display starting status on our lander's OLED display.
+   *
+   * The U8g2 library for Arduino uses a technique called "page buffering" to draw on
+   * displays connected to microcontrollers with limited RAM. Here's a short explanation
+   * of how it works:
+   *
+   * firstPage(): This function starts the drawing routine. It initializes the buffer with
+   *              the first "page" or segment of the display.
+   * nextPage(): After drawing the first page, this function is called in a loop. Each call
+   *             loads the next segment into the buffer and allows you to draw additional
+   *             content. This process repeats until the entire display content is drawn over
+   *             multiple pages.
+   * Looping: The combination of firstPage() and nextPage() is typically used inside a loop.
+   *          The loop continues until nextPage() returns false, indicating that all pages
+   *          have been drawn.
+   *
+   * By drawing the display in segments, the library conserves RAM, making it feasible to use
+   * graphics displays with microcontrollers that have limited memory resources.
+   */
   lander_display.firstPage();
   do {
     byte y_offset = drawString(0, 0, "Exploration Lander");
-    y_offset = drawString(0, y_offset, "Liftoff Sequence");
+    drawString(0, y_offset, "Liftoff Sequence");
+
+    // Status on bottom line of OLED display
     drawString(0, lander_display.getDisplayHeight() - lander_display.getMaxCharHeight(), "Countdown Active");
+    // Draw a picture of our lander int bottom right corner
     displayLander(lander_display.getDisplayWidth() - LANDER_WIDTH,
                   lander_display.getDisplayHeight() - LANDER_HEIGHT);
   } while (lander_display.nextPage());
 
-  // blink starting value 3 times
+  // blink the countdown on our timer before beginning the countdown
   for (int i = 0; i < 4; i++) {
     counter_display.clear();
     delay(200);
-    displayCounter(COUNTDOWN_TIME);
+    displayCounter(COUNTDOWN_MILLISECONDS);
     delay(200);
   }
   Serial.println("Countdown started..: ");
@@ -113,45 +154,55 @@ void setup() {
 
 // *********************************************
 void loop() {
-  static unsigned long timeRemaining = COUNTDOWN_TIME;
+  // Initialize static variables first time through loop() that will retain values
+  // between loop() executions.
+  static unsigned long timeRemaining = COUNTDOWN_MILLISECONDS;
   static unsigned long countdown_start_time = millis();
 
-  Serial.println(timeRemaining);
-  displayCounter(timeRemaining);
+  Serial.println(timeRemaining);  // Display milliseconds remaining on serial console
+  displayCounter(timeRemaining);  // Display minutes:seconds on counter display
 
+  // If timeRemaining has reached 0 display ending values
   if (timeRemaining == 0) {
-    Serial.println("Done!!");
-    counter_display.setSegments(DONE);
+    Serial.println("Done!!");           // indicate completion on serial console
+    counter_display.setSegments(DONE);  // "dOnE" on our counter
+
+    // Now update our OLED display with ending screen using firstPage()/nextPage()
     lander_display.firstPage();
     do {
+      // Each time we display a line of text on our display the y_offset
+      // is updated to point to the next available point for drawing.
+      // Display first two lines
       byte y_offset = drawString(0, 0, "Exploration Lander");
       y_offset = drawString(0, y_offset, "Liftoff ABORTED");
 
-      // lander_display.setFontPosCenter();  // Y coordinate relative to center of font height
+      // Set y_offset to point four lines above bottom of display
       y_offset = lander_display.getDisplayHeight() - (4 * lander_display.getMaxCharHeight());
+      // Display last four lines
       y_offset = drawString(0, y_offset, "Thrusters: OFF");
       y_offset = drawString(0, y_offset, "Systems: OFF");
       y_offset = drawString(0, y_offset, "Confirm: OFF");
-
-      // lander_display.setFontPosBottom();
       drawString(0, y_offset, "Countdown ABORT");
+      // Draw a picture of our lander int bottom right corner
       displayLander(lander_display.getDisplayWidth() - LANDER_WIDTH,
                     lander_display.getDisplayHeight() - LANDER_HEIGHT);
-      // draw();
     } while (lander_display.nextPage());
+
+    // Stop here by looping forever.
     while (1)
       ;
   }
 
-  // Update the time remaining
+  // Update our remaining time by subtracting the start time from current
+  // execution time (in milliseconds).  If our elapsed time is greater
+  // than our countdown time then set remaining time to 0.
   unsigned long elapsed_time = millis() - countdown_start_time;
-  if (elapsed_time < COUNTDOWN_TIME) {
-    timeRemaining = COUNTDOWN_TIME - elapsed_time;
+  if (elapsed_time < COUNTDOWN_MILLISECONDS) {
+    timeRemaining = COUNTDOWN_MILLISECONDS - elapsed_time;
   } else {
     timeRemaining = 0;
   }
 }
-// *********************************************
 
 // Display milliseconds as minutes:seconds (MM:SS)
 void displayCounter(unsigned long milliseconds) {
@@ -165,29 +216,12 @@ void displayCounter(unsigned long milliseconds) {
   counter_display.showNumberDecEx(seconds, 0, true, 2, 2);
 }
 
-// Use the .drawStr() method to draw a string in the display centered
-// horizontally between the given X coordinate and the maximum X.
-// Y coordinate is unchanged and text is displayed relative to the
-// current font positioning mode (Top, Center, Bottom).
-// byte drawCenteredString(byte x, byte y, char *string) {
-//   byte centered_x = x + ((lander_display.getDisplayWidth() - x) - lander_display.getStrWidth(string)) / 2;
-//   lander_display.drawStr(centered_x, y, string);
-// }
-
+// Draw test on our lander display at x, y, returning new y
+// value that is immediately below the new line of text.
 byte drawString(byte x, byte y, char *string) {
-  lander_display.drawStr(0, y, string);
+  lander_display.drawStr(x, y, string);
   return (y + lander_display.getMaxCharHeight());  // return new y_offset on display
 }
-
-// OLED Display output
-// void draw(void) {
-//   // graphic commands to redraw the complete screen should be placed here
-//   lander_display.setFont(u8g_font_6x10);
-
-
-//   lander_display.drawStr(0, 22, "LIFTOFF!!");
-//   lander_display.drawStr(0, 44, "End of sim");
-// }
 
 // Draw an image of our lander drawn with frames and triangles
 // at location x_location, y_location (relative to the upper left corner).
